c6b7af4a76b7d11a2edc8d4d63304d52fb673fe5
Caren Nafula Wakuloba <canafula16@gmail.com>
2023-05-01 03:11:47 +0300

    Display change

--- bot-battler/src/components/YourBotArmy.js
+++ bot-battler/src/components/YourBotArmy.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function YourBotArmy ({ bots, removeBot, releaseBot}) {
+function YourBotArmy ({ removeBot, releaseBot}) {
     function handleDelete(bot) {
         removeBot(bot)
     }
@@ -11,17 +11,6 @@ function YourBotArmy ({ bots, removeBot, releaseBot}) {
     
     return <div className="collection" onClick={() => handleRelease}>
             <h3>Army Display</h3>
-            {bots.map(bot=> {
-                return <div key={bot.id} className="bot-in-army" >
-                <h3>{bot.name} Display</h3>
-                <img src={bot.avatar_url} alt={bot.name} />
-                <div><h3>Health:{bot.health}</h3></div>
-                <div><h3>Damage:{bot.damage}</h3></div>
-                <div><h3>Armor:{bot.armor}</h3></div>
-                <div><h3>Bot_class:{bot.bot_class}</h3></div>
-                <button onClick={() => handleDelete(bot)}>X</button>
-                </div>
-            })}
     </div>
 }
 export default YourBotArmy
